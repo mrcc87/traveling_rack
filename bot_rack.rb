0290886ccbf8f2755b8ec62b3cde3a02058d2a0b
Mircea Samuila <mrcc87@gmail.com>
2018-07-06 18:25:19 +0200

    added net and ip start

--- bot_rack.rb
+++ bot_rack.rb
@@ -1,11 +1,32 @@
 #!/usr/bin/env ruby
 
 require 'rack'
+require 'net/http'
+
 
 class Greeter
 
   def call(env)
-    Rack::Response.new("Hello World")
+    request = Rack::Request.new(env)
+    case request.path
+    when "/"
+      Rack::Response.new("BOT")
+    when "/oauth"
+      body = "{ \"name\": \"#{request.params['username']}\", \"password\": \"#{request.params['password']}\", \"res\": \"#{call_site}\" }"
+      Rack::Response.new(body, 200, {'Content-Type' => 'application/json'})
+    else
+    Rack::Response.new("Not Found", 404)
+    end
+  end
+
+  def call_site
+    uri = URI('https://www.mirceasamuila.com/')
+    req = Net::HTTP::Get.new(uri)
+    res = Net::HTTP::start(uri.host, uri.port, :use_ssl => uri.scheme == "https") do |http|
+      http.request(req)
+    end
+
+    res['server']
   end
 end
-Rack::Handler::WEBrick.run Greeter.new, :Port => 3000
+Rack::Handler::WEBrick.run Greeter.new, :Port => 3000 #, :Host => "192.168.1.3"
